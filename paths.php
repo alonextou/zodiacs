7dacd5a829c300243bcab2a38bdec9685916786f
Alex Crawford <alex@j8mzfv1-linux.domain>
2012-12-02 13:40:47 -0800

    paths

--- paths.php
+++ paths.php
@@ -1,8 +1,12 @@
 <?php
 
+// This sets our working directory to where this file is located.
+chdir(__DIR__);
+
+// This can give our other files the working directory path.
 $GLOBALS['base'] = __DIR__;
 
-chdir(__DIR__);
+// This starts our application!
 require 'libraries/bootstrap.php';
 $bootstrap = new Bootstrap();
 
